f020b75cf12c7c98aa2f24397b9038c496e47aef
Borewit <borewit@users.noreply.github.com>
2020-01-01 18:21:12 +0100

    Export parseContentRange function

--- lib/index.ts
+++ lib/index.ts
@@ -2,6 +2,7 @@ import { IRangeRequestClient, IRangeRequestConfig } from './types';
 import { RangeRequestFactory } from './range-request-factory';
 import { ITokenizer } from 'strtok3/lib/core';
 export { IRangeRequestClient, IRangeRequestResponse, IContentRangeType, IHeadRequestInfo, IRangeRequestConfig } from './types';
+export { parseContentRange } from './range-request-tokenizer';
 
 /**
  * Construct range-tokenizer from range-request-client and config
